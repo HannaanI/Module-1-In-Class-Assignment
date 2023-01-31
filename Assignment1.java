39998ab762494a0718a969e0f5b9e577b26afd4f
HannaanI <123582452+HannaanI@users.noreply.github.com>
2023-01-30 23:41:51 -0500

    1st Commit

---
+++ Assignment1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.assignment1;
+import java.util.Scanner;
+/**
+ *
+ * @author hannaan
+ */
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter upto five numbers: ");
+        int fivenumbers = input.nextInt();
+        System.out.printf("%3s %3s %3s %3s %3s", "4", "2", "3", "3", "9");
+  
+    }
+}
+        
